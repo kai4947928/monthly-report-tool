1afee4d30c4695b991726c79d70745dea2e506c3
Kaito Ono <kaito@example.com>
2026-06-07 20:13:33 +0900

    cost and profit aggregation

--- app/aggregator.py
+++ app/aggregator.py
@@ -1,7 +1,12 @@
-def aggregate_store_data(sales_df, tax_rate_master_df):
+def aggregate_store_data(sales_df, tax_rate_master_df, cost_master_df):
+
+    #1ヶ月の税抜売上
     sales_amount_tax_ex = sales_df["sales_amount_tax_ex"].sum()
+
+    #1ヶ月の来客数
     customer_count = sales_df["customer_count"].sum()
 
+    #利用できる消費税
     active_tax_rate = tax_rate_master_df.loc[
         tax_rate_master_df["is_active"] == 1,
         "tax_rate"
@@ -9,6 +14,46 @@ def aggregate_store_data(sales_df, tax_rate_master_df):
 
     tax_amount = sales_amount_tax_ex * active_tax_rate
     sales_amount_tax_in = sales_amount_tax_ex + tax_amount
+
+    #対象店舗
+    store_code = sales_df["store_code"].iloc[0]
+    store_cost = cost_master_df.loc[
+        cost_master_df["store_code"] == store_code
+    ]
+
+    #原価
+    cost_rate = store_cost["cost_rate"].iloc[0]
+    cost_amount = sales_amount_tax_ex * cost_rate
+
+    #人件費
+    labor_cost_rate = store_cost["labor_cost_rate"].iloc[0]
+    labor_cost_amount = (
+        sales_amount_tax_ex * labor_cost_rate
+    )
+
+    #経費
+    rent_cost = store_cost["rent_cost"].iloc[0]
+    utility_cost = store_cost["utility_cost"].iloc[0]
+    other_expense_cost = store_cost["other_expense_cost"].iloc[0]
+    expense_amount = (
+        rent_cost + utility_cost + other_expense_cost
+    )
+
+    #粗利
+    gross_profit = (
+        sales_amount_tax_ex - cost_amount
+    )
+
+    net_profit = (
+        sales_amount_tax_ex - cost_amount - labor_cost_amount - expense_amount
+    )
+
+    if sales_amount_tax_ex == 0:
+        profit_rate = 0
+    else:
+        profit_rate = (
+            net_profit / sales_amount_tax_ex
+        ) * 100
 
     if customer_count == 0:
         average_spend = 0
@@ -22,6 +67,12 @@ def aggregate_store_data(sales_df, tax_rate_master_df):
         "customer_count": customer_count,
         "average_spend": average_spend,
         "tax_rate": active_tax_rate,
+        "cost_amount": cost_amount,
+        "labor_cost_amount": labor_cost_amount,
+        "expense_amount": expense_amount,
+        "gross_profit": gross_profit,
+        "net_profit": net_profit,
+        "profit_rate": profit_rate,
     }
 
     return result
--- app/main.py
+++ app/main.py
@@ -6,11 +6,13 @@ from app.aggregator import aggregate_store_data
 target_month = "202606"
 csv_path = INPUT_DIR / target_month / "sales_001_202606_20260524_091533.csv"
 csv_tax_rate_master_path = MASTER_DIR / "tax_rate_master_20260529.csv"
+cost_master_path = MASTER_DIR / "cost_master_20260529.csv"
 
 sales_df = load_csv(csv_path)
 tax_rate_master_df = load_csv(csv_tax_rate_master_path)
+cost_master_df = load_csv(cost_master_path)
 
-result = aggregate_store_data(sales_df, tax_rate_master_df)
+result = aggregate_store_data(sales_df, tax_rate_master_df, cost_master_df)
 
 print(result)
 
