2fa951d84b3c7c7d5deca93bd358a7c6d5259692
Kaito Ono <kaito@example.com>
2026-06-07 21:05:55 +0900

    monthly sales CSV loader

--- app/csv_loader.py
+++ app/csv_loader.py
@@ -1,6 +1,21 @@
 import pandas as pd
+from app.config import INPUT_DIR
 
 def load_csv(file_path):
     df = pd.read_csv(file_path)
 
     return df
+
+def load_monthly_sales_csv(target_month):
+    input_dir = INPUT_DIR / target_month
+    csv_files = list(input_dir.glob("*.csv"))
+
+    sales_df_list = []
+
+    for csv_file in csv_files:
+        df = pd.read_csv(csv_file)
+        sales_df_list.append(df)
+
+    monthly_sales_df = pd.concat(sales_df_list, ignore_index=True)
+
+    return monthly_sales_df
--- app/main.py
+++ app/main.py
@@ -1,18 +1,14 @@
-from app.config import INPUT_DIR, MASTER_DIR
-from app.csv_loader import load_csv
-from app.aggregator import aggregate_store_data
+from app.csv_loader import load_monthly_sales_csv
 
 
-target_month = "202606"
-csv_path = INPUT_DIR / target_month / "sales_001_202606_20260524_091533.csv"
-csv_tax_rate_master_path = MASTER_DIR / "tax_rate_master_20260529.csv"
-cost_master_path = MASTER_DIR / "cost_master_20260529.csv"
+def main():
+    target_month = "202606"
 
-sales_df = load_csv(csv_path)
-tax_rate_master_df = load_csv(csv_tax_rate_master_path)
-cost_master_df = load_csv(cost_master_path)
+    monthly_sales_df = load_monthly_sales_csv(target_month)
 
-result = aggregate_store_data(sales_df, tax_rate_master_df, cost_master_df)
+    print(monthly_sales_df.head())
+    print(monthly_sales_df.shape)
 
-print(result)
 
+if __name__ == "__main__":
+    main()
